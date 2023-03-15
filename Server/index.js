c8b62a9cf5586d4bfb4d3a4346ef8fab36dbb58e
yuvrajcell-99 <yyyuvraj11@gmail.com>
2023-03-15 23:54:16 +0530

    ..

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,15 @@
-import { Express } from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors'
 import helmet from "helmet";
 import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js"
+import managementRoutes from "./routes/management.js"
+import salesRoutes from "./routes/sales.js"
+
+
 
 
 /*Confguration*/
@@ -11,10 +17,23 @@ import morgan from "morgan";
 
 dotenv.config();
 const app=express();
-app.use(express.json())
-app.use(helmet())
+app.use(express.json());
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
-app.use(morgan("common"))
+app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(cors())
+app.use(cors());
+
+
+
+/*Routes*/
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management",managementRoutes);
+app.use("/sales", salesRoutes);
+
+
+
+
